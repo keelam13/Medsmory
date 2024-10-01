affda7a0f8aada31130f1745c6f57ef9117930a4
Keevin Aroco <gavikde2022@gmail.com>
2024-10-01 23:41:09 +0000

    Add event listener to start button and enter questions and options to quiz data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
-function startQuiz() {
+document.addEventListener("DOMContentLoaded", function() {
+    const startBtn = document.getElementsByClassName("start-btn")[0];
+    
+    startBtn.addEventListener("click", startQuiz);
+})
 
+function startQuiz() {
+    console.log("Started!");
 }
 
 function showQuestion() {
@@ -21,3 +27,106 @@ function addScore() {
 function showResult() {
     
 }
+
+const quizData = [
+    {
+        question: "This drug blocks the action of a protein in the kidneys called sodium-glucose co-transporter 2 (SGLT2), which stops the kidneys passing glucose from the blood into the urine. Thereby causing the kidneys to pass more glucose into the urine and reducing the glucose level in the blood. It also increases the elimination of salt and water in the urine.", 
+        options: ["Dapagliflozin (Forxiga)", "Melperone (Bunil)", "Escitalopram (Lexapro)"],
+        answer: "Dapagliflozin (Forxiga)"
+    },
+    {
+        question:"It is a corticosteroid that may be used to reduce inflammation and calm down an overactive immune system. It works by mimicking the effects of cortisol, a hormone released by the adrenal glands that regulates metabolism and stress. It prevents the release of substances in the body that cause inflammation.",
+        options:["Amitriptyline (Elavil)","Prednisolone (Okrido)","Dapagliflozin (Forxiga)"],
+        answer:"Prednisolone (Okrido)"
+    },
+    {
+        question:"An atypical butyrophenone antipsychotic used to treat sleep disorders, confusion, and psychomotor dysfunction associated with geriatric, psychiatric, and alcohol-dependent patients.",
+        options:["Melperone (Bunil)","Apixaban (Eliquis)","Amiodarone (Cordarex)"],
+        answer:"Melperone (Bunil)"
+    },
+    {
+        question:"It is a calcium channel blocker which inhibits the contractile processes of the myocardial smooth muscle cells, causing dilation of the coronary and systemic arteries, increased oxygen delivery to the myocardial tissue, decreased total peripheral resistance, decreased systemic blood pressure, and decreased afterload.",
+        options:["Lercanidipin (Zanidip)","Safinamide (Xadago)","Ondansetron (Zofran)"],
+        answer:"Lercanidipin (Zanidip)"
+    },
+    {
+        question:"An antidepressant used to treat major depressive episodes. It is a 'selective serotonin reuptake inhibitor' (SSRI). It works by preventing the neurotransmitter serotonin from being taken back up into nerve cells in the brain and spinal cord, thus making more serotonin available to help pass messages between brain cells. Low levels of serotonin in the central nervous system are thought to be associated with depression or anxiety.",
+        options:["Melperone (Bunil)","Pregabalin (Lyrica)","Escitalopram (Lexapro)"],
+        answer:"Escitalopram (Lexapro)"
+    },
+    {
+        question:"It is a semi-synthethic macrolide antibiotic against certain Gram-negative bacteria, particularly Legionella pneumophila. It binds to the bacterial ribosome and interferes with bacterial protein synthesis.",
+        options:["Amiodarone (Cordarex)","Roxithromycin (Rulid)","Melperone (Bunil)"],
+        answer:"Roxithromycin (Rulid)"
+    },
+    {
+        question:"A factor Xa inhibitor, an anticoagulant. It works by decreasing the clotting ability of the blood and helps prevent harmful clots from forming in the blood vessels.",
+        options:["Apixaban (Eliquis)","Dapagliflozin (Forxiga)","Hydrochlorothiazide (Esidrix)"],
+        answer:"Apixaban (Eliquis)"
+    },
+    {
+        question:"It belongs to the group of medicines known as antiarrhythmics. It works directly on the heart tissue and will slow the nerve impulses in the heart. This helps keep your heart rhythm normal.",
+        options:["Escitalopram (Lexapro)","Amiodarone (Cordarex)","Safinamide (Xadago)"],
+        answer:"Amiodarone (Cordarex)"
+    },
+    {
+        question:"A combination adrenergic antagonist and serotonin agonist given orally for the treatment of essential hypertension and intravenously in hypertensive emergencies.",
+        options:["Urapidil (Ebrantil)","Ondansetron (Zofran)","Tocilizumab (Actemra)"],
+        answer:"Urapidil (Ebrantil)"
+    },
+    {
+        question:"This is a type of diuretic used alone or together with other medicines to treat high blood pressure. It decreases sodium reabsorption which increases fluid loss in urine, which in turn decreases extracellular fluid and plasma volume.",
+        options:["Hydrochlorothiazide (Esidrix)","Dimenhydrinate (Vomex)","Pregabalin (Lyrica)"],
+        answer:"Hydrochlorothiazide (Esidrix)"
+    },
+    {
+        question:"Used to treat symptoms of depression. It works on the central nervous system (CNS) to increase levels of certain chemicals in the brain. This medicine is a tricyclic antidepressant (TCA). It is suggested that it  inhibits the membrane pump mechanism responsible for the re-uptake of transmitter amines, such as norepinephrine and serotonin, thereby increasing their concentration at the synaptic clefts of the brain improving depressive symptoms.",
+        options:["Safinamide (Xadago)","Amitriptyline (Elavil)","Roxithromycin (Rulid)"],
+        answer:"Amitriptyline (Elavil)"
+    },
+    {
+        question: "It is used in combination with levodopa and carbidopa to treat adults with Parkinson's disease who are having 'off' episodes. This medicine is a MAO-B inhibitor that works to increase and extend the effects of levodopa and may help to slow the progress of Parkinson's disease.",
+        options:["Lercanidipin (Zanidip)","Insulin lispro (Humalog)","Safinamide (Xadago)"],
+        answer:"Safinamide (Xadago)"
+    },
+    {
+        question:"Works in the central nervous system (CNS) to control seizures and pain. It is an anticonvulsant and neuropathic pain agent.",
+        options:["Amitriptyline (Elavil)","Amiodarone (Cordarex)","Pregabalin (Lyrica)"],
+        answer:"Pregabalin (Lyrica)"
+    },
+    {
+        question:"This a combination drug indicated to prevent nausea, vomiting, and dizziness caused by motion sickness. The antiemetic properties of dimenhydrinate are primarily thought to be produced by its antagonism of H1 histamine receptors in the vestibular system, while the excitatory effects are thought to be produced by its adenosine receptor blockade.",
+        options:["Tocilizumab (Actemra)","Dimenhydrinate (Vomex)","Ondansetron (Zofran)"],
+        answer:"Dimenhydrinate (Vomex)"
+    },
+    {
+        question:"Used to prevent nausea and vomiting that is caused by cancer medicines or radiation therapy. It is also used to prevent nausea and vomiting that may occur after surgery. It works in the stomach to block the signals to the brain that cause nausea and vomiting.",
+        options:["Ondansetron (Zofran)","Fentanyl (Duragesic)","Amitriptyline (Elavil)"],
+        answer:"Ondansetron (Zofran)"
+    },
+    {
+        question:"A synthetic opioid used for the management of postoperative pain. It binds to and activates mu-opioid receptors in the central nervous system, thereby mimicking the effects of endogenous opioids and producing analgesic relief.",
+        options:["Piritramide (Dipidolor)","Tocilizumab (Actemra)","Safinamide (Xadago)"],
+        answer:"Piritramide (Dipidolor)"
+    },
+    {
+        question:"A strong opioid analgesic that acts on the central nervous system (CNS) to relieve pain. It is used to treat severe and persistent pain that requires an extended treatment period and when other pain medicines did not work well enough or cannot be tolerated.",
+        options:["Fentanyl (Duragesic)","Amiodarone (Cordarex)","Melperone (Bunil)"],
+        answer:"Fentanyl (Duragesic)"
+    },
+    {
+        question:"Rapid-acting form of insulin used for the treatment of hyperglycemia caused by Type 1 and Type 2 Diabetes. It mimics the activity of endogenously produced human insulin promoting the uptake of glucose from the blood into internal organs and tissues for its transformation into glycogen or fat for storage.",
+        options:["Apixaban (Eliquis)","Insulin lispro (Humalog)","Dapagliflozin (Forxiga)"],
+        answer:"Insulin lispro (Humalog)"
+    },
+    {
+        question:"Used as a source of iron in patients with iron deficiency anemia with chronic kidney disease, including those who are undergoing dialysis and those who do not require dialysis. Due to less side effects than iron dextran, iron sucrose is more preferred in chronic kidney disease patients.",
+        options:["Piritramide (Dipidolor)","Hydrochlorothiazide (Esidrix)","Iron sucrose (FerMed)"],
+        answer:"Iron sucrose (FerMed)"
+    },
+    {
+        question:"A recombinant humanized monoclonal antibody IL-6 receptor inhibitor used to treat inflammatory and autoimmune conditions. It is indicated to treat moderate to severe rheumatoid arthritis, giant cell arteritis, systemic sclerosis-associated interstitial lung disease, polyarticular juvenile idiopathic arthritis, systemic juvenile idiopathic arthritis, and cytokine release syndrome.",
+        options:["Tocilizumab (Actemra)","Piritramide (Dipidolor)","Safinamide (Xadago)"],
+        answer:"Tocilizumab (Actemra)"
+    },
+]
